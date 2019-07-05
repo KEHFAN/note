3df88222977a2f2309721a09e5bab520b9276d67
KEHFAN <33714955+KEHFAN@users.noreply.github.com>
2019-07-05 21:14:46 +0800

    Update miss.cpp

--- miss.cpp
+++ miss.cpp
@@ -1,6 +1,6 @@
 //1065 sum may be overflow  :a>0&b>0  a+b<0 ,a<0&b<0  a+b>=0
 //1052 静态链表：考虑无用节点的情况   node[p]={p,key,next,false};
-//1047 超时问题：cin cout能避免就避免，字符串输入scanf("%s",str);str为字符数组；操作时可将str转为string类型：
+//1047 超时问题：cin cout能避免就避免，字符串输入scanf("%s",str);str为字符数组；操作时可将str转为string类型：fdsgf
 //1041 acc 提交时注意选择正确的编译语言
 //1034 DFS:  MAX上界 MAX命名可能会冲突，使用大写 熟练运用DFS 邻接矩阵存储要考虑对称操作 G[u][v]=0;G[v][u]=0;
 //1032 静态链表：效率问题，节点中增加flag 将一个链表中节点置true，逐个访问另一个节点时遇到true即得到相同点
