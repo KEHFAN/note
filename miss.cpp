0c02dd70dadb84b5c0f1df3747714c6e50adc3ce
KEHFAN <33714955+KEHFAN@users.noreply.github.com>
2019-07-11 21:07:09 +0800

    Update miss.cpp

--- miss.cpp
+++ miss.cpp
@@ -12,7 +12,7 @@
 //1047 超时问题：cin cout能避免就避免，字符串输入scanf("%s",str);str为字符数组；操作时可将str转为string类型：string=str;str=string.c_str();
 //1044 acc 如果涉及排序 可优先考虑 map<int,map<int,int> >
 //1041 acc 提交时注意选择正确的编译语言
-//1039 acc 能用map转化排序问题 就尽量不要用
+//1039 acc 使用字符串hash速度更快 能用map转化排序问题 就尽量不要用
 //1034 DFS:  MAX上界 MAX命名可能会冲突，使用大写 熟练运用DFS 邻接矩阵存储要考虑对称操作 G[u][v]=0;G[v][u]=0;
 //1032 静态链表：效率问题，节点中增加flag 将一个链表中节点置true，逐个访问另一个节点时遇到true即得到相同点
 //1028 acc
